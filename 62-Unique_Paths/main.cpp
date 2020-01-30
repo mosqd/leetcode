ecb45be28b6b0eacac3141ad5894ced15b0bdc13
mosqd <TryCatch.exe@gmail.com>
2020-01-30 19:34:50 +0800

    problem 62

---
+++ 62-Unique_Paths/main.cpp
@@ -0,0 +1,55 @@
+/**
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+ */
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if (!m || !n) {
+            return 0;
+        }
+        int **dp = new int*[n];
+        int i = 0, j = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = new int[m];
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = 0;
+            }
+        }
+        for (j = 0; j < m; j++) {
+            dp[0][j] = 1;
+        }
+        for (i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+        for (i = 1; i < n; i++) {
+            for (j = 1; j < m; j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
