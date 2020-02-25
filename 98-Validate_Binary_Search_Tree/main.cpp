fc3841885f3df6637c9bf27bc94b5d167cd68a7f
mosqd <TryCatch.exe@gmail.com>
2020-02-25 19:44:49 +0800

    problem 98

---
+++ 98-Validate_Binary_Search_Tree/main.cpp
@@ -0,0 +1,64 @@
+/**
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    Solution() : _node(nullptr) {}
+    bool isValidBST(TreeNode* root) {
+        return inOrder(root);
+    }
+    bool inOrder(TreeNode* root) {
+        if (root) {
+            if (!inOrder(root->left)) {
+                return false;
+            }
+            if (_node && _node->val >= root->val) {
+                return false;
+            }
+            _node = root;
+            if (!inOrder(root->right)) {
+                return false;
+            }
+        }
+        return true;
+    }
+private:
+    TreeNode *_node;
+};
